41621b4d32218d51ab26ad44b6024daaa15ecd40
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-09-21 10:27:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const status = document.getElementById("status");
 const enterBtn = document.getElementById("enterBtn");
 
 enterBtn.addEventListner("click", ()=> {
-	status.innerHTML = "<h1 id="status">Entered Metaverse</h1>";
+	// status.innerHTML = "<h1 id="status">Entered Metaverse</h1>";
+	let h1 = document.createElement("h1");
+	h1.innerText = "Entered Metaverse";
+	h1.id = "status";
+	document.body.insertBefore(h1, enterBtn)
+	status.remove();
+		
 })
